06e4ff7f606de1e69242fb3d4d387714d179e7fc
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2024-04-17 11:00:25 -0400

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -1,6 +1,6 @@
 class Sprite extends Animation {
     constructor(atlas, sprites, pos, size, speed, disp = _renderer, playing = true, progress = 0, step = 0) {
-        super(disp, playing, progress, step, speed, true, false, 0);
+        super(disp, playing, progress, step, speed, true, false, disp.frameRate());
         this.atlas = atlas;
         this.sprites = sprites;
         this.pos = pos;
@@ -14,7 +14,7 @@ class Sprite extends Animation {
                     this.sprites.length - 1 :
                     Math.floor((this.sprites.length - 1) * stepProgress)
             ];
-        image(this.atlas,
+        disp.image(this.atlas,
               this.pos[0], this.pos[1], this.size[0], this.size[1],
               this.sprite[0] * this.atlas.width, this.sprite[1] * this.atlas.height,
               this.sprite[2] * this.atlas.width, this.sprite[3] * this.atlas.height);
