224f4f0595c04fc73d6a0e86170c56f14f154688
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2024-04-17 09:30:52 -0400

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -8,7 +8,12 @@ class Sprite extends Animation {
     }
 
     animationFrame(stepProgress) {
-        this.sprite = this.sprites[Math.floor((this.sprites.length - 1) * stepProgress)];
+        this.sprite = 
+            this.sprites[
+                stepProgress == 1 ?
+                    this.sprites.length - 1 :
+                    Math.floor((this.sprites.length - 1) * stepProgress)
+            ];
         image(this.atlas,
               this.pos[0], this.pos[1], this.size[0], this.size[1],
               this.sprite[0] * this.atlas.width, this.sprite[1] * this.atlas.height,
